d2ae5e5b8cb93c7b4dd1c7edf8f360ce1f2f4a5a
lunaplush <lunaplus@mail.ru>
2022-10-05 22:55:51 +0300

    ch lib

--- lib1.py
+++ lib1.py
@@ -11,11 +11,13 @@ def convert_to_float(name):
 
     return a
 
-df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1589241600000_86400000_1000.csv", index_col=['timestamp'], parse_dates=['price'])
+#df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1589241600000_86400000_1000.csv", index_col=['timestamp'], parse_dates=['price'])
+df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1664668800000_86400000_1873.csv", index_col=['timestamp'], parse_dates=['price'])
 df["price"] = df["price"].apply(convert_to_float)
 plt.plot(df.index, df.price)
-lb = [a[0:10] for a in df.index[0:1000:99]]
-plt.xticks(np.linspace(0,1000,11), lb, rotation="vertical")
-lb = ["{:.0f}".format(i) for i in np.linspace(3000, 20000, 34)]
-plt.yticks(np.linspace(3000, 20000, 34), lb, rotation="horizontal")
+n = len(df.price)
+lb = [a[0:10] for a in df.index[0:n:99]]
+plt.xticks(np.linspace(0, n, len(lb)), lb, rotation="vertical")
+lb = ["{:.0f}".format(i) for i in np.linspace(df.price.min(), df.price.max(), 34)]
+plt.yticks(np.linspace(df.price.min(), df.price.max(), 34), lb, rotation="horizontal")
 plt.show()
