2c073062985ee9caa6cb145b333bc47e05f512bf
lunaplush <lunaplus@mail.ru>
2022-10-09 23:51:56 +0300

    View Data from File, OpenFile Menu

--- lib1.py
+++ lib1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 def convert_to_float(name):
     a = name.replace(",",".")
     try:
@@ -11,13 +12,34 @@ def convert_to_float(name):
 
     return a
 
-#df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1589241600000_86400000_1000.csv", index_col=['timestamp'], parse_dates=['price'])
-df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1664668800000_86400000_1873.csv", index_col=['timestamp'], parse_dates=['price'])
-df["price"] = df["price"].apply(convert_to_float)
-plt.plot(df.index, df.price)
-n = len(df.price)
-lb = [a[0:10] for a in df.index[0:n:99]]
-plt.xticks(np.linspace(0, n, len(lb)), lb, rotation="vertical")
-lb = ["{:.0f}".format(i) for i in np.linspace(df.price.min(), df.price.max(), 34)]
-plt.yticks(np.linspace(df.price.min(), df.price.max(), 34), lb, rotation="horizontal")
-plt.show()
+def open_data(path):
+    try:
+        df = pd.read_csv(path, index_col=['timestamp'],
+                         parse_dates=['price'])
+        df["price"] = df["price"].apply(convert_to_float)
+        return df
+    except:
+        return []
+def draw_data(df, ax):
+    ax.clear()
+    ax.plot(df.index, df.price)
+    n = len(df.price)
+    lb = [a[0:10] for a in df.index[0:n:99]]
+    ax.set_xticks(np.linspace(0, n, len(lb)), lb, rotation="vertical")
+    lb = ["{:.0f}".format(i) for i in np.linspace(df.price.min(), df.price.max(), 34)]
+    ax.set_yticks(np.linspace(df.price.min(), df.price.max(), 34), lb, rotation="horizontal")
+    ax.figure.canvas.draw()
+    ax.figure.canvas.show()
+
+if __name__ == '__main__':
+    #     #df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1589241600000_86400000_1000.csv", index_col=['timestamp'], parse_dates=['price'])
+    df  = pd.read_csv("data/BTCUSDT_1d_1502928000000-1664668800000_86400000_1873.csv", index_col=['timestamp'], parse_dates=['price'])
+    df["price"] = df["price"].apply(convert_to_float)
+    plt.plot(df.index, df.price)
+    n = len(df.price)
+    lb = [a[0:10] for a in df.index[0:n:99]]
+    plt.xticks(np.linspace(0, n, len(lb)), lb, rotation="vertical")
+    lb = ["{:.0f}".format(i) for i in np.linspace(df.price.min(), df.price.max(), 34)]
+    plt.yticks(np.linspace(df.price.min(), df.price.max(), 34), lb, rotation="horizontal")
+    plt.tight_layout()
+    plt.show()
--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from PyQt5.QtCore import QAbstractTableModel, Qt, QCoreApplication
 from PyQt5 import uic
 import sys
 import os
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+import lib1
 
 
 class PdTable(QAbstractTableModel):
@@ -38,32 +42,56 @@ class PdTable(QAbstractTableModel):
 
 
 class mainApp(QMainWindow):
+
     def __init__(self):
         super(mainApp, self).__init__()
         uic.loadUi("base.ui", self)
-        a = []
+        self.createMenus()
         self.btnClose.clicked.connect(QCoreApplication.instance().quit)
-        self.chekPandasWork()
+        self.figure = plt.figure(figsize=(30, 20), facecolor="#FFFFFF")
+        #self.figure.tight_layout()
+        self.canvas = FigureCanvas(self.figure)
+        self.figure.tight_layout()
+        self.graph.addWidget(self.canvas)
+        self.ax = self.figure.subplots(1, 1)
+        #self.viewData()
+        self.actionOpenFile.triggered.connect(self.onOpenFile)
 
-    def chekPandasWork(self):
-        data = {'Пол': ['мужской', 'женский', 'женский', 'мужской', 'мужской'],
-                'Имя': ['Сяо Мин', 'Сяо Хун', 'Сяо Фан', 'Сяо Цян', 'Сяо Мэй'],
-                "Возраст": [20, 21, 25, 24, 29]
-                }
+    def createMenus(self):
+        menubar = self.menuBar()
 
-        df = pd.DataFrame(data, index=['No.1', 'No.2', 'No.3', 'No.4', 'No.5'],
-                          columns=['Имя', 'Пол', 'Возраст', 'Род занятий'])
-        model = PdTable(df)
-        view = self.tableView
-        view.setModel(model)
-        view.setWindowTitle('Pandas')
-        view.setAlternatingRowColors(True)
-        view.show()
+    def onOpenFile(self):
+        file = QFileDialog.getOpenFileName()
+        if file:
+           self.viewData(file[0])
+
+    def viewData(self, file = "data/BTCUSDT_1d_1502928000000-1589241600000_86400000_1000.csv" ):
+        # data = {'Пол': ['мужской', 'женский', 'женский', 'мужской', 'мужской'],
+        #         'Имя': ['Сяо Мин', 'Сяо Хун', 'Сяо Фан', 'Сяо Цян', 'Сяо Мэй'],
+        #         "Возраст": [20, 21, 25, 24, 29]
+        #         }
+        #
+        # df = pd.DataFrame(data, index=['No.1', 'No.2', 'No.3', 'No.4', 'No.5'],
+        #                   columns=['Имя', 'Пол', 'Возраст', 'Род занятий'])
+
+        #df = lib1.open_data("data/BTCUSDT_1d_1502928000000-1664668800000_86400000_1873.csv")
+        df = lib1.open_data(file)
+        if len(df):
+            model = PdTable(df)
+            view = self.tableView
+            view.setModel(model)
+            view.setWindowTitle('Pandas')
+            view.setAlternatingRowColors(True)
+            view.show()
+            lib1.draw_data(df, self.ax)
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     window = mainApp()
-    window.chekPandasWork()
+   # window.viewData()
     window.show()
     app.exec_()
